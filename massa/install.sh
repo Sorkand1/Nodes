44102249652ec5e82190707793d3e7b7dc331158
Andrii Soroka <96412234+sorkand1@users.noreply.github.com>
2022-09-22 14:13:14 +0300

    Update install.sh

--- massa/install.sh
+++ massa/install.sh
@@ -14,28 +14,6 @@ rm -rf $HOME/massa.tar.gz
 chmod +x $HOME/massa/massa-node/massa-node \
 $HOME/massa/massa-client/massa-client
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n massa_password
-. <(wget -qO- https://raw.githubusercontent.com/SecorD0/Massa/main/insert_variables.sh)
-. <(wget -qO- https://raw.githubusercontent.com/SecorD0/Massa/main/multi_tool.sh) \
--rb
-sudo tee <<EOF >/dev/null /etc/systemd/system/massad.service
-[Unit]
-Description=Massa Node
-After=network-online.target
-
-[Service]
-User=$USER
-WorkingDirectory=$HOME/massa/massa-node
-ExecStart=$HOME/massa/massa-node/massa-node -p "$massa_password"
-Restart=on-failure
-RestartSec=3
-LimitNOFILE=65535
-
-[Install]
-WantedBy=multi-user.target
-EOF
-sudo systemctl daemon-reload
-sudo systemctl enable massad
-sudo systemctl restart massad
 sleep 1
 echo "Весь необходимый софт установлен"
 echo "-----------------------------------------------------------------------------"
