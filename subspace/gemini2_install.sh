1c2aa7a30835fe8d92fbf31a4e24aa743d69c4fc
Andrii Soroka <96412234+sorkand1@users.noreply.github.com>
2022-09-23 07:50:30 +0300

    Update gemini2_install.sh

--- subspace/gemini2_install.sh
+++ subspace/gemini2_install.sh
@@ -72,7 +72,7 @@ After=network.target
 [Service]
 User=$USER
 Type=simple
-ExecStart=/usr/local/bin/subspace-farmer farm --reward-address $SUBSPACE_WALLET --plot-size 110G
+ExecStart=/usr/local/bin/subspace-farmer farm --reward-address $SUBSPACE_WALLET --plot-size 100G
 Restart=on-failure
 LimitNOFILE=65535
 
