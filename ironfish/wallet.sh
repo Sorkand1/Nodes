3f5d851f02d332a9b66764e38a5a521d0218ecd6
Andrii Soroka <96412234+sorkand1@users.noreply.github.com>
2022-09-24 20:45:56 +0300

    Create wallet.sh

---
+++ ironfish/wallet.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Створення імя ноди
+if [ ! $myname ]; then
+	read -p "Введите имя вашей ноды : " myname
+fi
+echo 'Имя вашей ноды: ' $myname
+sleep 1
+echo "Готово"
+echo "-----------------------------------------------------------------------------"
+ironfish accounts:create $myname
+ironfish accounts:use myname
+ironfish config:set nodeName myname
+ironfish config:set blockGraffiti myname
+ironfish config:set minerBatchSize 60000
+ironfish config:set enableTelemetry true
+echo "Имя ноды успешно добавлено"
+echo "-----------------------------------------------------------------------------"
