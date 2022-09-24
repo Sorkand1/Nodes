fa8f47e84c4937ffe688364b088491d09131b330
Andrii Soroka <96412234+sorkand1@users.noreply.github.com>
2022-09-24 20:59:02 +0300

    Create wallet2.sh

---
+++ ironfish/wallet2.sh
@@ -0,0 +1,4 @@
+ironfish config:set nodeName $IRONFISH_NODENAME
+ironfish config:set blockGraffiti $IRONFISH_NODENAME
+ironfish accounts:create $IRONFISH_WALLET
+ironfish accounts:use $IRONFISH_WALLET
