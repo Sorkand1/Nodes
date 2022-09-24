3ffe11e58ae22ea1d0293499408c544352dc0c3f
Andrii Soroka <96412234+sorkand1@users.noreply.github.com>
2022-09-24 20:49:22 +0300

    Update wallet.sh

--- ironfish/wallet.sh
+++ ironfish/wallet.sh
@@ -9,9 +9,9 @@ sleep 1
 echo "Готово"
 echo "-----------------------------------------------------------------------------"
 ironfish accounts:create $myname
-ironfish accounts:use myname
-ironfish config:set nodeName myname
-ironfish config:set blockGraffiti myname
+ironfish accounts:use $myname
+ironfish config:set nodeName $myname
+ironfish config:set blockGraffiti $myname
 ironfish config:set minerBatchSize 60000
 ironfish config:set enableTelemetry true
 echo "Имя ноды успешно добавлено"
