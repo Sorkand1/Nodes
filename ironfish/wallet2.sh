5ab58da1fb5f0510b41cfaae7c0b1f698bb8ee42
Andrii Soroka <96412234+sorkand1@users.noreply.github.com>
2022-09-24 21:01:26 +0300

    Delete wallet2.sh

--- ironfish/wallet2.sh
+++
@@ -1,4 +0,0 @@
-ironfish config:set nodeName $IRONFISH_NODENAME
-ironfish config:set blockGraffiti $IRONFISH_NODENAME
-ironfish accounts:create $IRONFISH_WALLET
-ironfish accounts:use $IRONFISH_WALLET
