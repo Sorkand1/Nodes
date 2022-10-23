b256698a6399a170f6c37edae275acfad01a6a47
Andrii Soroka <96412234+sorkand1@users.noreply.github.com>
2022-10-23 10:39:21 +0300

    Update install.sh

--- manta/install.sh
+++ manta/install.sh
@@ -1,1 +1,21 @@
 
+#!/bin/bash
+echo "-----------------------------------------------------------------------------"
+curl -s https://raw.githubusercontent.com/sorkand1/nodes/main/welcome.sh | bash
+echo "-----------------------------------------------------------------------------"
+echo "-----------------------------------------------------------------------------"
+echo "Устанавливаем софт"
+echo "-----------------------------------------------------------------------------"
+sudo apt install make clang pkg-config libssl-dev libclang-dev build-essential git curl ntp jq llvm tmux htop screen unzip cmake -y
+curl -s https://raw.githubusercontent.com/sorkand1/tools/main/install_rust.sh | bash &>/dev/null
+source $HOME/.cargo/env
+source $HOME/.profile
+source $HOME/.bashrc
+sleep 1
+echo "Клонируем репозиторий, начинаем билд"
+echo "-----------------------------------------------------------------------------"
+git clone https://github.com/Manta-Network/manta-rs.git
+cd manta-rs
+cargo run --release --package manta-trusted-setup --all-features --bin groth16_phase2_client register
+echo "Билд закончен, переходим к инициализации ключей"
+echo "-----------------------------------------------------------------------------"
