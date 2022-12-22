d8718976af020c6784ab124876bc3b7be1c71bd8
Andrii Soroka <96412234+sorkand1@users.noreply.github.com>
2022-12-22 12:37:49 +0200

    Update install.sh

--- anoma/install.sh
+++ anoma/install.sh
@@ -3,7 +3,7 @@
 echo "-----------------------------------------------------------------------------"
 curl -s https://raw.githubusercontent.com/sorkand1/nodes/main/welcome.sh | bash
 echo "-----------------------------------------------------------------------------"
-
+sleep 1
 apt update &>/dev/null
 apt install git &>/dev/null
 apt install apt-transport-https ca-certificates software-properties-common curl &>/dev/null
@@ -13,6 +13,6 @@ apt update &>/dev/null
 cd /root &>/dev/null
 sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y &>/dev/null
 sudo systemctl status docker &>/dev/null
-
+sleep 2
 echo "Нода добавлена в автозагрузку на сервере, запущена"
 echo "-----------------------------------------------------------------------------"
