cb6bc6b8e6c5e3648726c5838a3d684954350c7f
Andrii Sorkand <96412234+sorkand1@users.noreply.github.com>
2023-02-25 10:06:14 +0200

    Create delete.sh

---
+++ ironfish/delete.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function logo {
+  bash <(curl -s https://raw.githubusercontent.com/sorkand1/nodes/main/welcome.sh)
+}
+
+function line {
+  echo "-----------------------------------------------------------------------------"
+}
+
+function colors {
+  GREEN="\e[1m\e[32m"
+  RED="\e[1m\e[39m"
+  NORMAL="\e[0m"
+}
+
+function docker_down {
+  docker-compose down
+}
+
+function docker_delete {
+  rm -rf .ironfish
+}
+
+
+colors
+line
+logo
+line
+docker_down
+docker_pull
+docker_delete
+line
